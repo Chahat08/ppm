a6c4407390e264e0c4bfaf4a9c32f8c09089430b
Chahat Kalsi <chahat.ck88@gmail.com>
2023-03-25 22:29:28 +0530

    resources added

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include "image/image.h"
 
 int main() {
-	Image image("resources/example4.ppm");
-	image.printImageOnConsole();
+	Image image1("resources/example1_p3.ppm");
+	image1.printImageOnConsole();
+
+	Image image2("resources/example_p2.ppm");
+	image2.printImageOnConsole();
+
+	//Image image3("resources/example_p3.ppm");
+	//image3.printImageOnConsole();
 }
